24b5c82a3d65a9ef19833abea63521ab3c70ffad
jonakemon <jonathan@seib.nl>
2020-12-14 20:26:25 +0100

    another test with beagle

--- rfid/__init__.py
+++ rfid/__init__.py
@@ -1,7 +1,4 @@
 __version__ = "2.2.1"
 
-try:
-    from rfid.rfid import RFID
-    from rfid.util import RFIDUtil
-except RuntimeError:
-    print("Must be used on Raspberry Pi")
+from rfid.rfid import RFID
+from rfid.util import RFIDUtil
--- rfid/rfid.py
+++ rfid/rfid.py
@@ -2,24 +2,15 @@ import threading
 
 RASPBERRY = object()
 BEAGLEBONE = object()
-board = RASPBERRY
-try:
-    # Try with Raspberry PI imports first
-    import spidev
-    import RPi.GPIO as GPIO
-    SPIClass = spidev.SpiDev
-    def_pin_rst = 22
-    def_pin_irq = 18
-    def_pin_mode = GPIO.BOARD
-except ImportError:
-    # If they failed, try with Beaglebone
-    import Adafruit_BBIO.SPI as SPI
-    import Adafruit_BBIO.GPIO as GPIO
-    SPIClass = SPI.SPI
-    board = BEAGLEBONE
-    def_pin_rst = "P9_23"
-    def_pin_irq = "P9_15"
-    def_pin_mode = None
+
+# If they failed, try with Beaglebone
+import Adafruit_BBIO.SPI as SPI
+import Adafruit_BBIO.GPIO as GPIO
+SPIClass = SPI.SPI
+board = BEAGLEBONE
+def_pin_rst = "P9_23"
+def_pin_irq = "P9_15"
+def_pin_mode = None
 
 class RFID(object):
     pin_rst = 22
