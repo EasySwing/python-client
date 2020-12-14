24ff3c0c25979a38aa1566079e61013424d1b00d
jonakemon <jonathan@seib.nl>
2020-12-14 14:32:08 +0100

    test

--- core.py
+++ core.py
@@ -1,11 +1,26 @@
 from api_wrapper import APIWrapper
 from config import Config
 
+import Adafruit_BBIO.GPIO as GPIO
+import time
+
 api = APIWrapper(Config)
 tag_uuid = '2b2f48c2-74e6-4d6b-98c1-111630bfa7ae'
 user = api.sign_in(tag_uuid)
 
-print(user.get('first_name'))
-print(user.get('last_name'))
-for club in user.get('clubs'):
-    print(club.get("name"), club.get("tee_height"), 'mm')
+name_length = len(user.get('first_name'))
+
+out = "P8_8"
+GPIO.setup(out, GPIO.OUT)
+
+for i in range(0, name_length):
+    GPIO.output(out, GPIO.HIGH)
+    time.sleep(0.01)
+    GPIO.output(out, GPIO.LOW)
+    time.sleep(0.01)
+
+
+# print(user.get('last_name'))
+
+# for club in user.get('clubs'):
+#     print(club.get("name"), club.get("tee_height"), 'mm')
